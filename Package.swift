96c34ce4ba6262c8fd9ffe06fa04dc73ec99679f
timmson <timmson666@mail.ru>
2022-12-02 21:45:26 +0300

    Add Alamofire

--- Package.swift
+++ Package.swift
@@ -6,16 +6,15 @@ import PackageDescription
 let package = Package(
     name: "HelloSpm",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.6.1"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "Main",
-            dependencies: ["HelloSpm"]),
+            dependencies: [
+                "Alamofire",
+                "HelloSpm"
+            ]),
         .target(
             name: "HelloSpm"),
         .testTarget(
--- Sources/Main/Main.swift
+++ Sources/Main/Main.swift
@@ -1,11 +1,15 @@
+import Alamofire
 import HelloSpm
 
 @main
 public final class Main {
 
     public static func main() {
+        AF.request("https://example.com").response {
+            response in debugPrint(response)
+        }
+
         print(HelloSpm().hello(name: "Who"))
-        //fetchAllRooms()
     }
 
 }
